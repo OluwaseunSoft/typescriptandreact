413dd45a7a9a0c85b101367f2f5e154ee4c9e81c
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2021-01-09 22:38:34 +0100

    commit second

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -7,7 +7,7 @@ import React from "react";
 export const TextField: React.FC<{text: string}> = () => {
     return(
         <div>
-            <input></input>
+            <input>What to enter right now</input>
         </div>
     )
 }
